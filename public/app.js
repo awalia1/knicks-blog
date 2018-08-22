0b531a14f417b64b2349f8059379a3eff382a91b
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-08-22 00:40:44 -0400

    fixed stylings on login and sign up page

--- public/app.js
+++ public/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     var x = document.getElementById('letsGo');
     setInterval(function() {
-        x.style.display = (f.style.display == 'none' ? '' : 'none');
+        x.style.display = (x.style.display == 'none' ? '' : 'none');
     }, 1000); 
 });
 
 $(document).ready(function() {
     var x = document.getElementById('teamName');
     setInterval(function() {
-        x.style.display = (f.style.display == 'block' ? '' : 'block');
+        x.style.display = (x.style.display == 'block' ? '' : 'block');
     }, 1000);  
 });
 
